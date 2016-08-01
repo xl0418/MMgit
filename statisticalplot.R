9a863212a88861be76cc34d6eca9e581e079f1e0
Liang Xu <xl0418@gmail.com>
2016-08-01 15:50:47 +0200

    24-26 groups' box plot for different allo speciation rates

--- statisticalplot.R
+++ statisticalplot.R
@@ -7,30 +7,27 @@ source(file = fileplot)
 # boxplot of p-value and power of test for different
 # p = rep(0,40)
 # power = rep(0,40)
-
-
-
 p = NA
 power = NA
-for(j in 20:24){
+for(j in 24:26){
   file1 = paste(getwd(),"/Desktop/data/Modeltestgroup",j,sep = "")
 for(i in 1:100){
   if(j == 20) file = paste(file1,"/Testanalysis",i,".Rdata",sep = "")
   else file = paste(file1,"/analysis",i,".Rdata",sep = "")
   load(file = file)
   # print(paste("pvalue",i," = ",pvalue,"; poweroftest",i," = ",poweroftest,sep = "")) 
-  i <- 100*(j-20)+i
+  i <- 100*(j-24)+i
   p[i] <- pvalue
   power[i] <- poweroftest
 }
   print(length(p))
   print(length(power))
 }
-group = rep(c(1,0.8,0.5,5,0.3),each = 100)
+group = rep(c(0.2,0,0.5),each = 100)
 # group = rep(c(50,0.1,0.2,0.15,0.3,0.35,0.4,0.4,10,1,0.8,0.9,1),each = 10)
 p2 = cbind(group, p , power)
 p2 = as.data.frame(p2)
- plot_p <- ggplot(p2, aes(x=factor(group), y=p)) +geom_boxplot()+xlab("Migration rate")+ylab("p-value")+#geom_hline(yintercept = 0.05)
+ plot_p <- ggplot(p2, aes(x=factor(group), y=p)) +geom_boxplot()+xlab("Allopatric speciation rate")+ylab("p-value")+#geom_hline(yintercept = 0.05)
  geom_hline(aes(yintercept=0.05)) + geom_text(aes(0,0.05,label = 0.05, vjust = 0))
    # + geom_dotplot(binaxis='y', stackdir='center', dotsize=1,binwidth = )
 # p <- ggplot(p2, aes(x=factor(group), y=power)) +geom_boxplot()
