4085916516bf4ee82c231c905bc63641ed029bff
Liang Xu <xl0418@gmail.com>
2016-08-02 17:15:40 +0200

    3D plot

--- plottree.R
+++ plottree.R
@@ -1,7 +1,13 @@
-for(i in 1:100){
-  file = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup28/out",i,"sim.Rdata",sep = "")
+library(DDD)
+library(plotly)
+library(MASS)
+library(plot3D)
+plottree <- function(x,num){
+if(x == 1){
+  for(i in 1:100){
+  file = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/out",i,"sim.Rdata",sep = "")
   load(file = file)
-  pdffilename = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup28/tree",i,".pdf",sep = "")
+  pdffilename = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/tree",i,".pdf",sep = "")
   pdf(pdffilename,paper = "a4r", width = 29.7, height = 21)
   L = result$L
   phy = L2phylo(L)
@@ -11,4 +17,38 @@ for(i in 1:100){
   
   try(dev.off())
 
+}
+else (x==2){
+  data = NA
+  for(i in 1:100){
+    file = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/out",i,"sim.Rdata",sep = "")
+    load(file = file)
+   # pdffilename = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/tree",i,".pdf",sep = "")
+   # pdf(pdffilename,paper = "a4r", width = 29.7, height = 21)
+    L = result$L
+    tes = L2phylo(L,dropextinct = T)
+    brts= -unname(sort(branching.times(tes),decreasing = T))
+    print(length(brts))
+    data0 = cbind(i,brts,c(2:(length(brts)+1)))
+    data = rbind(data, data0)
+   # try(dev.off())
+   
+    
+  }
+  kd <- with(MASS::geyser, MASS::kde2d(duration, waiting, n = 50))
+  with(kd, plot_ly(x = data[,1], y = data[,2], z = data[,3], type = "surface"))
+  
+  
+  plot_ly(z = data, type = "surface")
+  
+  
+  x = data[,1]
+  y = data[,2]
+  z = data[,3]
+  scatter3D(x, y, z, clab = c("Number of", "lineages"))
+  
+}
+  
+  
+}
 }
