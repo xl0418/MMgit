f2de9e053948809c8f928da81ba3ca464bf2c942
Liang Xu <xl0418@gmail.com>
2016-08-04 12:52:09 +0200

    3D matrix for plot

--- plottree.R
+++ plottree.R
@@ -37,13 +37,32 @@ else if(x==2){
     data0 = cbind(i,brts,c(2:(length(brts)+1)))
     data = rbind(data, data0)
    # try(dev.off())
+    
   }
-  x = data[,1]
-  y = data[,2]
-  z = data[,3]
+  data <- data[-1,]
+  time = data[order(data[,2]),2]
+  timeu = time[-1,]
+  data_lineage = timeu
+  for(i in 1:100){
+    file = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/out",i,"sim.Rdata",sep = "")
+    load(file = file)
+    # pdffilename = paste(getwd(),"/Dropbox/R/cluster/Modeltestgroup",num,"/tree",i,".pdf",sep = "")
+    # pdf(pdffilename,paper = "a4r", width = 29.7, height = 21)
+    L = result$L
+    tes = L2phylo(L,dropextinct = T)
+    brts= -unname(sort(branching.times(tes),decreasing = T))
+    M1 = match(brts,timeu)
+    M1[1] = 1
+    M11 = diff(M1)
+    M13 = length(timeu)-max(M1)+1
+    M12 = c(M11,M13)
+    N1 = rep(2:(length(brts)+1),M12)
+    data_lineage = cbind(data_lineage,N1)
+  }
+  x = data_lineage[,1]
+  y = c(1:100)
+  z = data_lineage[,2:101]
   scatter3D(x, y, z,surf = TRUE, clab = c("Number of", "lineages"))
-  # scatterplot3d(x, y, z, type="h",highlight.3d=TRUE, col.axis="blue",col.grid="grey", main="scatterplot3d - 2", pch=16,)
-  # wireframe(data, aspect = c(61/87, 0.4), screen = list(z = 30, x = -60), xlab = "X", ylab = "Y", zlab = "Z")
-
+ 
     }
 }
